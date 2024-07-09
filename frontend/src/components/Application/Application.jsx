dc47ee80ed2f50a1501f28872642d4d09cd8f5d8
MD ALTAF AHMED <97396706+altafahmed94@users.noreply.github.com>
2024-07-09 20:54:42 +0530

    Update Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "https://job-seeking-application-frontend-vdmg.onrender.com/api/v1/application/post",
+        "https://job-seeking-application-backend-8upm.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
