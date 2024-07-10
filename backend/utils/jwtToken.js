7f8fe354624b4b2d70642ffded3fe9e1d569e33d
MD ALTAF AHMED <97396706+altafahmed94@users.noreply.github.com>
2024-07-10 20:51:28 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,6 +1,7 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
+    name : "altafahmedcookie",
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
